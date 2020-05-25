c5fff7d05f8f0450ceb87fa28bf596945ef0796f
John Nicholas <john.nicholas@dinoroar.io>
2020-05-25 23:17:29 +0100

    games add to a list

---
+++ DinoGame/Server/Hubs/GameListHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DinoGame.Server.Hubs
+{
+    public class GameListHub : Hub
+    {
+        public async Task GameListUpdate(string game)
+        {
+            await Clients.All.SendAsync("GameListUpdate", game);
+        }
+    }
+}
