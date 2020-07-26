1798ac8da29339cf7a9ff60c4a8167cebe927dd9
John Nicholas <john.nicholas@dinoroar.io>
2020-07-27 00:03:21 +0100

    added form to create a game

---
+++ DinoGame.Blazor/Pages/survival/CreateGameModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinoGame.Blazor.Pages.survival
+{
+    public class CreateGameModel
+    {
+        [Required]
+        [StringLength(32)]
+        public string GameName { get; set; }
+
+        [Required]
+        [Range(1,20)]
+        public int BoardSize { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberOfHerbivores { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberOfCarnivores { get; set; }
+    }
+}
